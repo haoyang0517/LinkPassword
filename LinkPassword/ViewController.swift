07ccd276e7b2ff28554fb2c9b75875834dd4c065
haoyang <yiphaoyang98@gmail.com>
2024-02-26 15:27:40 +0800

    Initial Commit

---
+++ LinkPassword/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LinkPassword
+//
+//  Created by Hao Yang Yip on 26/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LinkPasswordUITests/LinkPasswordUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LinkPasswordUITestsLaunchTests.swift
+//  LinkPasswordUITests
+//
+//  Created by Hao Yang Yip on 26/02/2024.
+//
+
+import XCTest
+
+final class LinkPasswordUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
